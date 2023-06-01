71350bff2fe2a33c72874c156196f373ce2e0ed0
imran khan <khanimran03466@gmail.com>
2023-06-01 22:51:03 +0530

    fix header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,50 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 
 const Header = () => {
 
   const [menuOpen, setMenuOpen] = useState(false);
-  const [navFixed, setNavFixed] = useState("");
+  const [navFixed, setNavFixed] = useState(false);
+  const [headerHeight, setHeaderHeight] = useState(0);
 
-  console.log(menuOpen);
+  useEffect(()=>{
+    window.addEventListener("scroll", e =>{
+      var doc = document.documentElement;
+      const top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+      if(top >= 75) {
+        setNavFixed(true)
+      } else {
+        setNavFixed(false)
+      }
+    } );
+
+    let headerHgt = document.querySelector("header").clientHeight;
+    setHeaderHeight(headerHgt);
+
+  },[])
+
+  
+
+  const handleMenuClick = (e) => {
+    e.preventDefault()
+    console.log(headerHeight);
+    const element = document.getElementById(`${e.target.getAttribute("href").replace("#", "")}`)
+    const y = element.getBoundingClientRect().top + window.scrollY - headerHeight;
+    window.scroll({
+      top: y,
+      behavior: 'smooth'
+    });
+  }
 
   return (
     <>
-      <header>
+      <header className={`${navFixed ? 'fixed-nav' : ''}`}>
         <nav>
           <div className="desk-nav">
-            <NavContent />
+            <NavContent handleMenuClick={handleMenuClick} />
           </div>
           <div className={`mob mob-nav ${menuOpen?"active":""}`}>
-            <MobileNav setMenuOpen={setMenuOpen} />
+            <MobileNav setMenuOpen={setMenuOpen} handleMenuClick={handleMenuClick} />
           </div>
         </nav>
 
@@ -28,23 +56,23 @@ const Header = () => {
   );
 };
 
-const MobileNav = ({setMenuOpen}) => (
+const MobileNav = ({setMenuOpen, handleMenuClick}) => (
   <>
     <div className="mobile-nav" onClick={ ()=> setMenuOpen(false) }>
-      <NavContent />
+      <NavContent handleMenuClick={handleMenuClick} />
     </div>
   </>
 );
 
-const NavContent = () => (
+const NavContent = ({handleMenuClick}) => (
   <>
     <h2>IMRAN.</h2>
     <div>
-      <a href="#home">Home</a>
-      <a href="#work">Work</a>
-      <a href="#experience">Experience</a>
-      <a href="#services">Services</a>
-      <a href="#contact">Contact</a>
+      <a href="#home" onClick={handleMenuClick}>Home</a>
+      <a href="#work" onClick={handleMenuClick}>Work</a>
+      <a href="#experience" onClick={handleMenuClick}>Experience</a>
+      <a href="#services" onClick={handleMenuClick}>Services</a>
+      <a href="#contact" onClick={handleMenuClick}>Contact</a>
     </div>
     <a href="mailto:khanimran03466@gamil.com">
       <button>Email</button>
